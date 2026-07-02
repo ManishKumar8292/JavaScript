6d86b64dfe9b326f6eb6a45105dfdbcb8ef29725
Manish Kumar <94685734+ManishKumar8292@users.noreply.github.com>
2026-07-02 15:36:27 +0000

    Operations

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -30,3 +30,40 @@ console.log(StringIsPassed, typeof StringIsPassed); //true string
 let marks = 33;
 let marksInString = String(marks);
 console.log(marksInString, typeof marksInString); //33 string
+
+/* <============= Operations  =============> */
+
+let value = 3;
+let negValue = -value;
+console.log(negValue); //-3
+
+console.log(2 + 2); //4
+console.log(2 - 2); //0
+console.log(2 * 2); //4
+console.log(2 ** 3); //8
+console.log(2 / 3); //0.6666666666666666
+console.log(4 % 3); //1
+
+let str1 = "Hello";
+let str2 = " Manish";
+let str3 = str1 + str2;
+console.log(str3); //Hello Manish
+
+console.log(1 + "2"); //12
+console.log("1" + 2); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "3"); //33
+
+console.log(+true); //1
+console.log(+false); //0
+console.log(+""); //0
+
+let num1, num2, num3, num4;
+num1 = num2 = num3 = num4 = 2 + 2;
+console.log(num2); //4
+
+let gameCount = 100;
+gameCount++;
+console.log(gameCount); //101
+++gameCount;
+console.log(gameCount); //102
