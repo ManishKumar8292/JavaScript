6966b606b76ca00c6d6b278bb33856e8db87a4c3
Manish Kumar <94685734+ManishKumar8292@users.noreply.github.com>
2026-07-02 14:07:49 +0000

    Data Types

---
+++ 01_Basics/02_Datatype.js
@@ -0,0 +1,34 @@
+//Number => 2 power 53
+//BigInt
+//String => "Manish"
+//boolean => true,false
+//symbol => unique
+//undefined
+//null => stonalone value
+
+let name = "Manish";
+console.log(typeof name); //string
+
+let age = 43;
+console.log(typeof age); //number
+
+let isLogin = true;
+console.log(typeof isLogin);
+
+let city;
+console.log(typeof city); //undefined
+
+let state = null;
+console.log(typeof state); //object
+
+//Object
+let user = {
+  name: "manish",
+  age: 29,
+};
+console.log(user, typeof user); //object
+
+//Array
+let fruits = ["apple", "lichi", "pineapple"];
+
+console.log(typeof fruits); //object
