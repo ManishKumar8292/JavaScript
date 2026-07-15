562c5efb1bd356a544d1d5f5b30de0463dbf318a
Manish Kumar <94685734+ManishKumar8292@users.noreply.github.com>
2026-07-15 13:40:16 +0000

    function part 1

---
+++ 03_Basics/01_Function.js
@@ -0,0 +1,42 @@
+function sayMyName() {
+  console.log("M");
+  console.log("A");
+  console.log("N");
+  console.log("I");
+  console.log("S");
+  console.log("H");
+}
+
+sayMyName();
+
+// function addTwoNumbers(num1, num2) {
+//   console.log(num1 + num2);
+// }
+// addTwoNumbers(5,6); //11
+
+
+function addTwoNumbers(num1, num2) {
+//   let result = num1+num2;
+//   return result ; 
+ return(num1 + num2);
+}
+
+const result=addTwoNumbers(5,6); 
+console.log(result); //11
+
+
+function printUser(user="sam"){
+    if(!user){
+        console.log("Please enter user name");
+        return;
+    }
+    return (`${user} Hello`)
+}
+const result1 =printUser("manish");
+console.log(result1); //manish Hello
+const result2 =printUser("");
+console.log(result2); // Hello
+const result3 =printUser();
+console.log(result3); //Please enter user name
+
+
