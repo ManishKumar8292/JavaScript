bdb2f1035cd77c1cc71d743d247156dbc3c1d64d
Manish Kumar <94685734+ManishKumar8292@users.noreply.github.com>
2026-07-16 10:04:26 +0000

    arrow

--- 03_Basics/03_arrow.js
+++ 03_Basics/03_arrow.js
@@ -12,3 +12,39 @@ const user = {
 user.welcomeMessage(); //Manish , Welcome To Website...
 user.username = "Sam";
 user.welcomeMessage(); //Sam , Welcome To Website...
+
+console.log(this); //in node environment is returns a empty object {}
+
+/* function chai() {
+  const username = "hitesh";
+  console.log(this); //it returns global object it means is a window object.
+  console.log(this.username); //undefined
+}
+chai();
+ */
+
+const chai = function () {
+  const username = "Hitesh";
+  console.log(this.username); //undefined
+};
+chai();
+
+const chai1 = () => {
+  const username = "Hietsh";
+  console.log(this); //{}
+  console.log(this.username); //undefined
+};
+chai1();
+
+// const addTwo = (n1, n2) => {
+//   return n1 + n2;
+// };
+
+// const addTwo = (n1, n2) => n1 + n2;
+
+const addTwo = (n1, n2) => n1 + n2;
+
+console.log(addTwo(10, 18));
+
+const returnObject = () => ({ username: "Manish" });
+console.log(returnObject()); //{ username: 'Manish' }
