2eb186a1183dd6b009e3c24e4b2d97769280febe
Manish Kumar <94685734+ManishKumar8292@users.noreply.github.com>
2026-07-20 14:15:37 +0000

    All selectors and style and convert into array

--- 06_DOM/one.js
+++ 06_DOM/one.js
@@ -33,3 +33,40 @@ document.querySelector('#subtitle');
 document.querySelectorAll('input')[0].style.padding='10px';
 document.querySelectorAll('input')[1].style.padding='10px';
 document.querySelector('input[type=password]'); //<input type="password" placeholder="Enter Your Password" style="padding: 10px;">
+
+document.querySelectorAll('input').forEach((item)=>{
+    item.style.border ='none';
+    item.style.outline="none";
+    item.style.fontSize="20px";
+    item.style.backgroundColor='#f2f2f2';
+})
+
+
+document.querySelector('ul li').style.color = "red";
+
+document.querySelectorAll('ul li')[2].style.color = 'green';
+
+const myUl = document.querySelector('ul');
+const myli = myUl.querySelector('li');
+console.log(myli)
+myli.style.backgroundColor="white";
+myli.style.padding="10px";
+myli.style.borderRadius="10px";
+
+myli.innerText = "First";
+
+
+const allLi = document.querySelectorAll('ul li');
+console.log(allLi); //NodeList(4) [li, li, li, li]
+
+const allListItems = document.getElementsByClassName('list-item');
+console.log(allListItems); //HTMLCollection(4) [li.list-item, li.list-item, li.list-item, li.list-item]
+
+const myConvertedList = Array.from(allListItems);
+console.log(myConvertedList); //(4) [li.list-item, li.list-item, li.list-item, li.list-item]
+
+myConvertedList.forEach((list)=>{
+    list.style.fontSize='20px';
+})
+
+
