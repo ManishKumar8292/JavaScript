4696d75baf0b1b050e35c541b79599d95ec159f7
Manish Kumar <94685734+ManishKumar8292@users.noreply.github.com>
2026-07-27 14:14:54 +0000

    Call

---
+++ 09_Classes _and_OOPS/Call.js
@@ -0,0 +1,12 @@
+function Setusername(myThis, username) {
+  myThis.username = username;
+}
+
+function CreateUserData(username, email, password) {
+  Setusername(this, username);
+  this.email = email;
+  this.password = password;
+}
+
+const chai = new CreateUserData("Manish", "manish@chai.com", "12334");
+console.log(chai); //CreateUserData { username: 'Manish', email: 'manish@chai.com', password: '12334' }
